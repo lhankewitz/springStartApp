7ed7e6d512400d6968bcd1deafdc4f269adc768c
Lutz Hankewitz <lumiha@Prometheus.fritz.box>
2016-02-26 13:04:44 +0100

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,18 @@
+/**
+ * Class to Initialize the App.
+ *
+ * @author L.Hankewitz
+ * @since 26/02/16.
+ */
+public class App {
+
+  public static void main(String[] args) {
+    App app = new App();
+
+    app.greeting();
+  }
+
+  public String greeting() {
+    return "Hello World!";
+  }
+}
